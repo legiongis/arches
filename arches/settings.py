ea6cb796d7d790b82362878babd2eb7035d3466b
Alexei Peters <apeters@fargeo.com>
2017-10-30 15:00:54 -0700

    add the ability to prefix elasticsearch index names, re #2612

--- arches/settings.py
+++ arches/settings.py
@@ -44,7 +44,6 @@ DATABASES = {
 # from http://django-guardian.readthedocs.io/en/stable/configuration.html#anonymous-user-name
 ANONYMOUS_USER_NAME = None
 
-
 ELASTICSEARCH_HTTP_PORT = 9200 # this should be in increments of 200, eg: 9400, 9600, 9800
 SEARCH_BACKEND = 'arches.app.search.search.SearchEngine'
 # see http://elasticsearch-py.readthedocs.org/en/master/api.html#elasticsearch.Elasticsearch
@@ -52,6 +51,8 @@ ELASTICSEARCH_HOSTS = [
     {'host': 'localhost', 'port': ELASTICSEARCH_HTTP_PORT}
 ]
 ELASTICSEARCH_CONNECTION_OPTIONS = {'timeout': 30}
+# a prefix to append to all elasticsearch indexes, note: must be lower case
+ELASTICSEARCH_PREFIX = ''
 
 # test settings for elasticsearch prefix work
 SEARCH_BACKEND = 'arches.app.search.search_prefix.SearchEngine'
