09d9d7eb441fd1c55620adb062aa2d14353f3c89
Adam Cox <mr.adamcox@gmail.com>
2025-07-31 22:32:54 +0000

    fix link to download file in email notification

--- arches/app/tasks.py
+++ arches/app/tasks.py
@@ -96,7 +96,7 @@ def export_search_results(self, userid, request_values, format, report_link):
             closing=_("Thank you"),
             email=email,
             name=export_name,
-            email_link=str(settings.ARCHES_NAMESPACE_FOR_DATA_EXPORT).rstrip("/") + "/files/" + str(search_history_obj.downloadfile),
+            email_link=str(settings.MEDIA_URL).rstrip("/") + "/" + str(search_history_obj.downloadfile),
         ),
     }
 
